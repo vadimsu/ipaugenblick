89f31eb2f4d55164e62b7a9eef9dbfd8a68e3c46
vadim <vadim@localhost.localdomain>
2014-09-19 10:25:38 -0400

    bug fixed - infinite loop

--- net/core/sock.c
+++ net/core/sock.c
@@ -1697,26 +1697,9 @@ EXPORT_SYMBOL(sock_kfree_s);
  */
 static long sock_wait_for_wmem(struct sock *sk, long timeo)
 {
-	DEFINE_WAIT(wait);
-
-	clear_bit(SOCK_ASYNC_NOSPACE, &sk->sk_socket->flags);
-	for (;;) {
-		if (!timeo)
-			break;
-		if (signal_pending(current))
-			break;
-		set_bit(SOCK_NOSPACE, &sk->sk_socket->flags);
-//		prepare_to_wait(sk_sleep(sk), &wait, TASK_INTERRUPTIBLE);
-		if (atomic_read(&sk->sk_wmem_alloc) < sk->sk_sndbuf)
-			break;
-		if (sk->sk_shutdown & SEND_SHUTDOWN)
-			break;
-		if (sk->sk_err)
-			break;
-		timeo = schedule_timeout(timeo);
-	}
-	finish_wait(sk_sleep(sk), &wait);
-	return timeo;
+//	clear_bit(SOCK_ASYNC_NOSPACE, &sk->sk_socket->flags);
+//	set_bit(SOCK_NOSPACE, &sk->sk_socket->flags);
+	return 0;
 }
 
 
