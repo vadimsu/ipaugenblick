28f012afec56b153668514e694d1517c020dcc3a
Vadim Suraev <vadim.suraev@gmail.com>
2015-01-13 17:03:33 +0200

    udp added

---
+++ service/test_client/build_test_client_udp.sh
@@ -0,0 +1,3 @@
+FLAGS=-g
+gcc $FLAGS -O -c ipaugenblick_main_udp.c -o ipaugenblick_main_udp.o
+gcc ipaugenblick_main_udp.o ../ipaugenblick_app_api/build/libipaugenblickservice.a ../../build/libnetinet.a ../../dpdk_libs/libdpdk.a  -lpthread -lrt -ldl -o test_client
---
+++ service/test_client/ipaugenblick_main_udp.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/errno.h>
+#include "../ipaugenblick_common/ipaugenblick_common.h"
+#include "../ipaugenblick_app_api/ipaugenblick_api.h"
+#include <string.h>
+
+int main(int argc,char **argv)
+{
+    void **buff;
+    int sock,len;
+    ipaugenblick_cmd_t *cmd;
+    char *p;
+    int size = 0,ringset_idx;
+    int socket_connected = -1;
+    unsigned int from_ip;
+    unsigned short from_port;
+
+    if(ipaugenblick_app_init(argc,argv) != 0) {
+        printf("cannot initialize memory\n");
+        return 0;
+    } 
+    if((sock = ipaugenblick_open_udp(inet_addr("192.168.150.63"),7777)) < 0) {
+        printf("cannot open UDP socket\n");
+        return 0;
+    }
+    while(1) {
+/*        if(socket_connected == -1) {
+            socket_connected = ipaugenblick_get_connected();
+        }
+        if(socket_connected == -1) {
+            continue;
+        }*/
+#if 0
+        buff = ipaugenblick_get_buffer(1448);
+        if(buff) {
+         printf("sending...\n");
+            if(ipaugenblick_sendto(sock,buff,0,1448,inet_addr("192.168.150.62"),7777)) { 
+                printf("failed\n");
+                ipaugenblick_release_tx_buffer(buff);
+                ipaugenblick_socket_kick(sock);
+            } 
+        }
+        else {
+//            ipaugenblick_socket_kick(sock);
+        }
+#endif
+#if 1
+        if(ipaugenblick_receivefrom(sock,&buff,&len,&from_ip,&from_port) == 0) {
+            printf("received %p\n",buff);
+            ipaugenblick_release_rx_buffer(buff);
+#if 0
+            buff = ipaugenblick_get_buffer(1448);
+            if(buff) {
+             printf("sending...\n");
+                if(ipaugenblick_sendto(sock,buff,0,1448,inet_addr("192.168.150.62"),7777)) { 
+                    printf("failed\n");
+                    ipaugenblick_release_tx_buffer(buff);
+                }
+            }
+            ipaugenblick_socket_kick(sock);
+#endif
+        }
+#endif
+    }
+    return 0;
+}
+
