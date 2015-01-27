95f4ac814a8ff0a40350d22a0010c05219b7a495
vadim.suraev@gmail.com <vadim.suraev@gmail.com>
2015-01-28 00:45:20 +0200

    deleting obsolete code

--- benchmark_app2/build_test.sh
+++
@@ -1,2 +0,0 @@
-rm -f bm
-gcc test.c ../build/libnetinet.a ../dpdk_libs/libdpdk.a build/lib/libperfm2 -lpthread -lrt -ldl -o bm
--- benchmark_app4/build_test.sh
+++
@@ -1,1 +0,0 @@
-gcc -g test.c ../build/libnetinet.a ../dpdk_libs/libdpdk.a build/lib/libperfm -lpthread -lrt -ldl
