cfb8dd6124e10c42a8b6b6e34189f04603b7f690
Vadim Suraev <vadim.suraev@gmail.com>
2015-05-17 16:21:17 +0300

    libraries are searched from the path

--- service/test_client/build_test_client_tcp_listener_with_select.sh
+++ service/test_client/build_test_client_tcp_listener_with_select.sh
@@ -1,4 +1,4 @@
 FLAGS=-Ofast
 #FLAGS=-g
 gcc $FLAGS -c ipaugenblick_main_tcp_listener_with_select.c -o ipaugenblick_main_tcp_listener_with_select.o
-gcc ipaugenblick_main_tcp_listener_with_select.o -L../ipaugenblick_app_api/build/lib -L../../dpdk-2.0.0/x86_64-native-linuxapp-gcc/lib -lipaugenblickservice -lrte_ring -lrte_timer -lrte_eal -lrte_mempool -lrte_malloc -lpthread -lrt -ldl -o test_client_tcp_with_select
+gcc ipaugenblick_main_tcp_listener_with_select.o -L../ipaugenblick_app_api/build/lib -lipaugenblickservice -lrte_ring -lrte_timer -lrte_eal -lrte_mempool -lrte_malloc -lpthread -lrt -ldl -o test_client_tcp_with_select
