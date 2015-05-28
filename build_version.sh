9e7a246a5fb64d3aa4bb9af07ecf2aa3dda92198
vadim.suraev@gmail.com <vadim.suraev@gmail.com>
2015-05-28 23:24:11 +0300

    show both untracked and changed

--- build_version.sh
+++ build_version.sh
@@ -3,6 +3,6 @@ echo '#ifndef __IPAUGENBLICK_SERVICE_BUILD_H__' > stack_and_service/ipaugenblick
 echo '#define __IPAUGENBLICK_SERVICE_BUILD_H__' >> stack_and_service/ipaugenblick_service_build.h
 echo '#define IPAUGENBLICK_SERVICE_BUILD \' >> stack_and_service/ipaugenblick_service_build.h
 git log -1 --pretty=oneline | gawk '{print "\"", $1, "\"" }' >> stack_and_service/ipaugenblick_service_build.h
-echo '#define IPAUGENBLICK_SERVICE_UNTRACKED \' >> stack_and_service/ipaugenblick_service_build.h
+echo '#define IPAUGENBLICK_SERVICE_UNTRACKED_AND_CHANGED \' >> stack_and_service/ipaugenblick_service_build.h
 git status --porcelain | wc | gawk '{print "\"", $1, "\"" }' >> stack_and_service/ipaugenblick_service_build.h
 echo '#endif /* __IPAUGENBLICK_SERVICE_BUILD_H__ */' >> stack_and_service/ipaugenblick_service_build.h
