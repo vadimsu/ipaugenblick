75ce36b44923a7ff2046ae57da851cbc2a3ee669
vadim.suraev@gmail.com <vadim.suraev@gmail.com>
2015-05-29 18:18:17 +0300

    build_dev script fixed

--- build_dev.sh
+++ build_dev.sh
@@ -2,11 +2,13 @@
 cd dpdk-2.0.0
 make install T=x86_64-native-linuxapp-gcc
 cd ..
-#rm -rf build
-make  CURRENT_DIR=$(pwd)/ clean
-make CURRENT_DIR=$(pwd)/
+#rm -rf build 
+make  CURRENT_DIR=$(pwd)/ clean RTE_SDK=$(pwd)/dpdk-2.0.0 LD_LIBRARY_PATH='stack_and_service/stack_and_service/x86_64-native-linuxapp-gcc/lib'
+make CURRENT_DIR=$(pwd)/ RTE_SDK=$(pwd)/dpdk-2.0.0 LD_LIBRARY_PATH='stack_and_service/stack_and_service/x86_64-native-linuxapp-gcc/lib'
 echo 'install libaries'
 sudo cp dpdk-2.0.0/x86_64-native-linuxapp-gcc/lib/*.so.* /usr/lib/.
 sudo cp stack_and_service/service/ipaugenblick_app_api/build/libipaugenblickservice.so /usr/lib/.
 echo 'install headers'
 sudo cp stack_and_service/service/ipaugenblick_app_api/ipaugenblick_api.h /usr/include/.
+sudo mkdir -p /etc/ipaugenblick
+sudo cp stack_and_service/service/dpdk_ip_stack_config.txt /etc/ipaugenblick
