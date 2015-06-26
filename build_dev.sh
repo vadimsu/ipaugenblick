298972772d1814edcd0724e3c28f554e47f924b5
Vadim Suraev <vadim.suraev@gmail.com>
2015-06-25 22:09:12 -0400

    fixed development build script

--- build_dev.sh
+++ build_dev.sh
@@ -8,6 +8,7 @@ make CURRENT_DIR=$(pwd)/ RTE_SDK=$(pwd)/dpdk-2.0.0 LD_LIBRARY_PATH='stack_and_se
 sudo cp  stack_and_service/service/stack_and_service/service/x86_64-native-linuxapp-gcc/ipaugenblick_srv /usr/bin/.
 echo 'install libaries'
 sudo mkdir -p /usr/lib/ipaugenblick
+sudo cp dpdk-2.0.0/x86_64-native-linuxapp-gcc/lib/*.so /usr/lib/ipaugenblick/.
 sudo cp dpdk-2.0.0/x86_64-native-linuxapp-gcc/lib/*.so.* /usr/lib/ipaugenblick/.
 sudo cp stack_and_service/service/ipaugenblick_app_api/stack_and_service/service/ipaugenblick_app_api/x86_64-native-linuxapp-gcc/lib/libipaugenblickservice.so /usr/lib/ipaugenblick/.
 sudo cp stack_and_service/stack_and_service/x86_64-native-linuxapp-gcc/lib/libnetinet.so /usr/lib/ipaugenblick/.
