2d76193d0beeafa4f33593f68861ff2adeadd266
vadimsu <vadim.suraev@gmail.com>
2015-11-15 09:59:37 -0500

    scripts to run examles

---
+++ stack_and_service/service/test_client/run_tcp_connect.sh
@@ -0,0 +1,1 @@
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/ipaugenblick ./test_client_tcp_with_select.bin -c 2 -n 1 --proc-type secondary
---
+++ stack_and_service/service/test_client/run_udp.sh
@@ -0,0 +1,1 @@
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/ipaugenblick ./test_client_tcp_udp.bin -c 2 -n 1 --proc-type secondary -- --ip2connect 192.168.150.62 --ip2bind 192.168.150.63 --port2bind 7777 --port2connect 7777
