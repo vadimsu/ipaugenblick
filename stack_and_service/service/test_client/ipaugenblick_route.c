81a81ac25d2f77f0bb11be1d3c5b9151677a0f11
vsuraev <vsuraev@northforgeinc.com>
2015-11-30 11:39:59 -0500

    fixed route entry configuration example

--- stack_and_service/service/test_client/ipaugenblick_route.c
+++ stack_and_service/service/test_client/ipaugenblick_route.c
@@ -13,7 +13,7 @@ int main(int argc,char **argv)
 {
     unsigned int dest_ip,mask,next_hop;
 
-    if(ipaugenblick_app_init(argc,argv) != 0) {
+    if(ipaugenblick_app_init(argc,argv,"route") != 0) {
         printf("cannot initialize memory\n");
         return 0;
     }
@@ -21,49 +21,51 @@ int main(int argc,char **argv)
     mask = inet_addr("255.255.255.0");
     next_hop = inet_addr("192.168.150.63");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop, 1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
     dest_ip = inet_addr("192.168.150.62");
     mask = inet_addr("255.255.255.255");
     next_hop = inet_addr("192.168.150.63");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop, 1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
+
     dest_ip = inet_addr("44.44.44.0");
     mask = inet_addr("255.255.255.0");
-    next_hop = inet_addr("192.168.150.62");
+    next_hop = inet_addr("192.168.151.62");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop,1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
-    dest_ip = inet_addr("55.55.5.0");
+    dest_ip = inet_addr("55.55.55.0");
     mask = inet_addr("255.255.255.0");
     next_hop = inet_addr("192.168.150.62");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop, 1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
+
     dest_ip = inet_addr("66.66.66.0");
     mask = inet_addr("255.255.255.0");
     next_hop = inet_addr("192.168.150.123");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop, 1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
     dest_ip = inet_addr("77.77.77.77");
     mask = inet_addr("255.255.255.255");
     next_hop = inet_addr("192.168.150.62");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop, 1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
     dest_ip = inet_addr("88.88.88.88");
     mask = inet_addr("255.255.255.255");
     next_hop = inet_addr("192.168.160.62");
     printf("add %x/%x nh %x\n",dest_ip,mask,next_hop);
-    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop)) {
+    if(ipaugenblick_add_v4_route(dest_ip,mask,next_hop, 1)) {
         printf("cannot add route %s %d\n",__FILE__,__LINE__);
     }
     dest_ip = inet_addr("44.44.44.0");
@@ -73,7 +75,7 @@ int main(int argc,char **argv)
     if(ipaugenblick_del_v4_route(dest_ip,mask,next_hop)) {
 	printf("cannot del route %s %d\n",__FILE__,__LINE__);
     }
-    dest_ip = inet_addr("55.55.5.0");
+    dest_ip = inet_addr("55.55.55.0");
     mask = inet_addr("255.255.255.0");
     next_hop = inet_addr("192.168.150.62");
     printf("del %x/%x nh %x\n",dest_ip,mask,next_hop);
@@ -101,6 +103,7 @@ int main(int argc,char **argv)
     if(ipaugenblick_del_v4_route(dest_ip,mask,next_hop)) {
 	printf("cannot del route %s %d\n",__FILE__,__LINE__);
     }
+
     return 0;
 }
 
