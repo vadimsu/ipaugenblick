37cd9e3607c11ccfa669be923b3ce49a83ff6b2b
vsuraev <vsuraev@northforgeinc.com>
2016-01-08 16:01:31 -0500

    fixed bugs in example

--- stack_and_service/service/test_client/ipaugenblick_main_tcp_listener_with_select.c
+++ stack_and_service/service/test_client/ipaugenblick_main_tcp_listener_with_select.c
@@ -14,7 +14,7 @@
 
 #define USE_TX 1
 #define USE_RX 1
-#define LISTENERS_COUNT 4
+#define LISTENERS_COUNT 8
 #define LISTENERS_BASE 7777
 
 static inline int is_listener(int sock, int *listeners)
@@ -144,7 +144,7 @@ int main(int argc,char **argv)
 	                ipaugenblick_set_socket_select(newsock,selector);
 			if (rxtxmask & 0x1)
 				ipaugenblick_fdset (newsock, &readfdset);
-			if (rxtxmask & 0x3)
+			if (rxtxmask & 0x2)
 				ipaugenblick_fdset (newsock, &writefdset);
 			ipaugenblick_fdset (newsock, &excfdset);
             	    }
@@ -170,7 +170,7 @@ int main(int argc,char **argv)
                 	}
 
 	                received_count+=segs;
-			if(!(received_count%1000)) {
+			if(!(received_count%1000000)) {
 	                    printf("received %u transmitted_count %u\n", received_count, transmitted_count);
 			    print_stats();
                 	}
@@ -195,7 +195,7 @@ int main(int argc,char **argv)
         	            break;
                 	}
 			transmitted_count++;
-			if(!(transmitted_count%1000)) {
+			if(!(transmitted_count%1000000)) {
                 	    printf("received %u transmitted_count %u\n", received_count, transmitted_count);
 			    print_stats();
         	        }
@@ -218,7 +218,7 @@ int main(int argc,char **argv)
 	                    }
         	            else {
 				transmitted_count += tx_space;
-				if(!(transmitted_count%1000)) {
+				if(!(transmitted_count%1000000)) {
         	            		printf("transmitted %u received_count %u\n", transmitted_count, received_count);
 					print_stats();
                 		}
--- stack_and_service/service/test_client/run_tcp_listener_with_select.sh
+++ stack_and_service/service/test_client/run_tcp_listener_with_select.sh
@@ -1,1 +1,1 @@
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/ipaugenblick ./test_client_tcp_with_select.bin -c 2 -n 1 --proc-type secondary
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/ipaugenblick ./test_client_tcp_with_select.bin -c 0xc -n 1 --proc-type secondary -- --notransmit
