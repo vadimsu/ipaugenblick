ec245a75de8237d3aef5fcf4c749b771f920c992
vsuraev <vsuraev@northforgeinc.com>
2016-01-08 15:42:11 -0500

    fixed app example

--- stack_and_service/service/ipaugenblick_service.sh
+++ stack_and_service/service/ipaugenblick_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/lib/ipaugenblick
-/usr/bin/ipaugenblick_srv -c 3 -n 1 -d librte_pmd_i40e.so -d librte_pmd_ixgbe.so -- -p 0x3 -l 0
+/usr/bin/ipaugenblick_srv -c 3 -n 4 -d librte_pmd_i40e.so -d librte_pmd_ixgbe.so -- -p 0x3 -l 0
 #/usr/bin/ipaugenblick_srv -c 3 -n 1 -- -p 0x3
